331acbeeb076b835d1f0cc5a13f11ea5cc812670
Christopher Fougner <chris@Christophers-MacBook-Pro.local>
2016-12-30 01:35:17 +0100

    fixed indent

--- examples/project_euler/problems.cpp
+++ examples/project_euler/problems.cpp
@@ -82,25 +82,25 @@ uint64_t problem7() {
 uint32_t problem9() {
   let n = 1000;
   return ftl::range(1, n / 2).map([](let a){
-      return ftl::range(1, a).map([a](let b){
-          return std::make_tuple(a, b);
-      });
-  })
-  .flat_map([n](let ab){
-      return std::tuple_cat(ab,
-          std::make_tuple(n - std::get<0>(ab) - std::get<1>(ab)));
-  })
-  .filter([](let abc) {
-      let a = std::get<0>(abc);
-      let b = std::get<1>(abc);
-      let c = std::get<2>(abc);
-      return a * a + b * b ==  c * c;
-  })
-  .map([](let abc){
-      return std::get<0>(abc) * std::get<1>(abc) * std::get<2>(abc);
-  })
-  .head()
-  .value();
+          return ftl::range(1, a).map([a](let b){
+              return std::make_tuple(a, b);
+          });
+      })
+      .flat_map([n](let ab){
+          return std::tuple_cat(ab,
+              std::make_tuple(n - std::get<0>(ab) - std::get<1>(ab)));
+      })
+      .filter([](let abc) {
+          let a = std::get<0>(abc);
+          let b = std::get<1>(abc);
+          let c = std::get<2>(abc);
+          return a * a + b * b ==  c * c;
+      })
+      .map([](let abc){
+          return std::get<0>(abc) * std::get<1>(abc) * std::get<2>(abc);
+      })
+      .head()
+      .value();
 }
 
 // Find the sum of all the primes below two million.
